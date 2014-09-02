195bea1be8a33c1b245bf1f52ba3636ba780e7f9
Abhi Sharma <abhisharma2@gmail.com>
2014-09-01 20:29:03 -0400

    setup dom

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,17 +3,53 @@
 angular.module('movieGameApp')
   .controller('movieController', function ($scope, $http) {
 
-      $timeout(function() {
-        $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/lists/movies/upcoming.json?page_limit=16&page=1&country=us', {
-            params: {
-                apikey: 'xvbv8w8sfb2wnp3um5erf6sw',
-                callback: 'JSON_CALLBACK'
-            }
-        })
-        .success(function (data) {
-            $scope.movies = data;
-        });
-      },0);
+      $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/movies/770672122.json', {
+          params: {
+              apikey: 'xvbv8w8sfb2wnp3um5erf6sw',
+              callback: 'JSON_CALLBACK'
+          }
+      })
+      .success(function (data) {
+          $scope.toy_story_3 = data;
+      });
 
+      $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/movies/13602.json', {
+          params: {
+              apikey: 'xvbv8w8sfb2wnp3um5erf6sw',
+              callback: 'JSON_CALLBACK'
+          }
+      })
+      .success(function (data) {
+          $scope.judge_dredd = data;
+      })
 
+      $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/movies/12161.json', {
+          params: {
+              apikey: 'xvbv8w8sfb2wnp3um5erf6sw',
+              callback: 'JSON_CALLBACK'
+          }
+      })
+      .success(function (data) {
+          $scope.the_abyss = data;
+      });
+
+      $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/movies/10223.json', {
+          params: {
+              apikey: 'xvbv8w8sfb2wnp3um5erf6sw',
+              callback: 'JSON_CALLBACK'
+          }
+      })
+      .success(function (data) {
+          $scope.spaceballs = data;
+      });
+
+      $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/movies/12911.json', {
+          params: {
+              apikey: 'xvbv8w8sfb2wnp3um5erf6sw',
+              callback: 'JSON_CALLBACK'
+          }
+      })
+      .success(function (data) {
+          $scope.godfather = data;
+      });
   });
