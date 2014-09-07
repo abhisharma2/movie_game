6b85f15e1d7598f86c346533f460186d6d8a6352
Abhi Sharma <abhisharma2@gmail.com>
2014-09-07 19:10:09 -0400

    game functionality and reject state

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,14 +1,20 @@
 'use strict';
 
 angular.module('movieGameApp')
-  .controller('movieController', function () {
+  .controller('movieController', function ($scope) {
     this.movies = data;
     this.current_movie = 1;
+    $scope.Math = window.Math;
+    this.show_score = 0;
   });
 
 angular.module('movieGameApp')
   .controller('gameController', function () {
     // Grab random movie
+
+    this.actual_score = 0;
+
+    debugger
 
     this.addScore = function(movie){
       movie.ratings.push(this.player_score);
@@ -16,8 +22,8 @@ angular.module('movieGameApp')
     };
 
     this.calculateActualScore = function(movie) {
-      this.actual_score = Math.abs(this.critics_score - this.player_score);
       movie.ratings.push(this.actual_score);
+      this.actual_score = 0;
     };
 
     this.overallGameScore = function(movies) {
@@ -67,6 +73,7 @@ angular.module('movieGameApp')
               {
                 id: 4,
                 title: "The Godfather",
+                year: 1972,
                 ratings: {
                   critics_score: 100,
                   player_score: 0,
@@ -76,6 +83,7 @@ angular.module('movieGameApp')
               {
                 id: 5,
                 title: "Spaceballs",
+                year: 1987,
                 ratings: {
                   critics_score: 50,
                   player_score: 0,
@@ -85,6 +93,7 @@ angular.module('movieGameApp')
               {
                 id: 6,
                 title: "Star Wars: Episode III - Revenge of the Sith 3D",
+                year: 2005,
                 ratings: {
                   critics_score: 80,
                   player_score: 0,
@@ -94,6 +103,7 @@ angular.module('movieGameApp')
               {
                 id: 7,
                 title: "The Lord of the Rings: The Return of the King",
+                year: 2003,
                 ratings: {
                   critics_score: 94,
                   player_score: 0,
@@ -103,6 +113,7 @@ angular.module('movieGameApp')
               {
                 id: 8,
                 title: "The Matrix",
+                year: 1999,
                 ratings: {
                   critics_score: 87,
                   player_score: 0,
@@ -112,6 +123,7 @@ angular.module('movieGameApp')
               {
                 id: 9,
                 title: "Transformers: Revenge of the Fallen",
+                year: 2009,
                 ratings: {
                   critics_score: 19,
                   player_score: 0,
@@ -121,6 +133,7 @@ angular.module('movieGameApp')
               {
                 id: 10,
                 title: "The Bourne Ultimatum",
+                year: 2007,
                 ratings: {
                   critics_score: 94,
                   player_score: 0,
@@ -130,6 +143,7 @@ angular.module('movieGameApp')
               {
                 id: 11,
                 title: "Harry Potter and the Order of the Phoenix",
+                year: 2007,
                 ratings: {
                   critics_score: 79,
                   player_score: 0,
@@ -139,6 +153,7 @@ angular.module('movieGameApp')
               {
                 id: 12,
                 title: "Shrek the Third",
+                year: 2007,
                 ratings: {
                   critics_score: 40,
                   player_score: 0,
@@ -148,6 +163,7 @@ angular.module('movieGameApp')
               {
                 id: 13,
                 title: "Sweeney Todd: The Demon Barber of Fleet Street",
+                year: 2007,
                 ratings: {
                   critics_score: 86,
                   player_score: 0,
@@ -157,6 +173,7 @@ angular.module('movieGameApp')
               {
                 id: 14,
                 title: "Pirates of the Caribbean: At Worlds End",
+                year: 2007,
                 ratings: {
                   critics_score: 45,
                   player_score: 0,
@@ -166,6 +183,7 @@ angular.module('movieGameApp')
               {
                 id: 15,
                 title: "Indiana Jones and the Kingdom of the Crystal Skull",
+                year: 2008,
                 ratings: {
                   critics_score: 78,
                   player_score: 0,
@@ -175,6 +193,7 @@ angular.module('movieGameApp')
               {
                 id: 16,
                 title: "The Dark Knight Rises",
+                year: 2012,
                 ratings: {
                   critics_score: 88,
                   player_score: 0,
@@ -184,6 +203,7 @@ angular.module('movieGameApp')
               {
                 id: 17,
                 title: "The Amazing Spider-Man",
+                year: 2012,
                 ratings: {
                   critics_score: 73,
                   player_score: 0,
@@ -192,6 +212,7 @@ angular.module('movieGameApp')
               },
               {
                 id: 18,
+                year: 2009,
                 title: "The Ugly Truth",
                 ratings: {
                   critics_score: 13,
@@ -202,6 +223,7 @@ angular.module('movieGameApp')
               {
                 id: 19,
                 title: "The Hunger Games",
+                year: 2012,
                 ratings: {
                   critics_score: 84,
                   player_score: 0,
