05137df3d7b4683a0e58ac6d5eacf41684d332b5
Abhi Sharma <abhisharma2@gmail.com>
2014-09-08 08:11:14 -0400

    share buttons for fb and twttr

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,32 +10,15 @@ angular.module('movieGameApp')
 
 angular.module('movieGameApp')
   .controller('gameController', function () {
-    // Grab random movie
-
-    this.actual_score = 0;
-
-    debugger
 
     this.addScore = function(movie){
       movie.ratings.push(this.player_score);
       this.player_score = 0;
     };
 
-    this.calculateActualScore = function(movie) {
-      movie.ratings.push(this.actual_score);
-      this.actual_score = 0;
-    };
-
-    this.overallGameScore = function(movies) {
-      var arry = []; //set movie array limit to 5?
-      var total = 0;
-      for (i=0;i<movies.length;i++) {
-        total += movies[i].ratings.actual_score;
-      }
-    };
-
-    this.gameScoreScale = function() {
-    };
+    // this.actualScore = function(movie) {
+      // movie.actual_score;
+    // };
 
   });
 
@@ -44,10 +27,10 @@ angular.module('movieGameApp')
                 id: 1,
                 title: "Charlie and the Chocolate Factory",
                 year: 2005,
+                actual_score: 0,
                 ratings: {
                   critics_score: 82,
-                  player_score: 0,
-                  actual_score: 82
+                  player_score: 0
                 }
               },
               {
@@ -56,178 +39,177 @@ angular.module('movieGameApp')
                 year: 2002,
                 ratings: {
                   critics_score: 72,
-                  player_score: 0,
-                  actual_score: 72
-                }
-              },
-              {
-                id: 3,
-                title: "The Incredibles",
-                year: 2004,
-                ratings: {
-                  critics_score: 97,
-                  player_score: 0,
-                  actual_score: 97
-                  }
-                },
-              {
-                id: 4,
-                title: "The Godfather",
-                year: 1972,
-                ratings: {
-                  critics_score: 100,
-                  player_score: 0,
-                  actual_score: 100
-                }
-              },
-              {
-                id: 5,
-                title: "Spaceballs",
-                year: 1987,
-                ratings: {
-                  critics_score: 50,
-                  player_score: 0,
-                  actual_score: 50
-                }
-              },
-              {
-                id: 6,
-                title: "Star Wars: Episode III - Revenge of the Sith 3D",
-                year: 2005,
-                ratings: {
-                  critics_score: 80,
-                  player_score: 0,
-                  actual_score: 80
-                }
-              },
-              {
-                id: 7,
-                title: "The Lord of the Rings: The Return of the King",
-                year: 2003,
-                ratings: {
-                  critics_score: 94,
-                  player_score: 0,
-                  actual_score: 94
-                }
-              },
-              {
-                id: 8,
-                title: "The Matrix",
-                year: 1999,
-                ratings: {
-                  critics_score: 87,
-                  player_score: 0,
-                  actual_score: 87
-                }
-              },
-              {
-                id: 9,
-                title: "Transformers: Revenge of the Fallen",
-                year: 2009,
-                ratings: {
-                  critics_score: 19,
-                  player_score: 0,
-                  actual_score: 19
-                }
-              },
-              {
-                id: 10,
-                title: "The Bourne Ultimatum",
-                year: 2007,
-                ratings: {
-                  critics_score: 94,
-                  player_score: 0,
-                  actual_score: 94
-                }
-              },
-              {
-                id: 11,
-                title: "Harry Potter and the Order of the Phoenix",
-                year: 2007,
-                ratings: {
-                  critics_score: 79,
-                  player_score: 0,
-                  actual_score: 79
-                }
-              },
-              {
-                id: 12,
-                title: "Shrek the Third",
-                year: 2007,
-                ratings: {
-                  critics_score: 40,
-                  player_score: 0,
-                  actual_score: 40
-                }
-              },
-              {
-                id: 13,
-                title: "Sweeney Todd: The Demon Barber of Fleet Street",
-                year: 2007,
-                ratings: {
-                  critics_score: 86,
-                  player_score: 0,
-                  actual_score: 86
-                }
-              },
-              {
-                id: 14,
-                title: "Pirates of the Caribbean: At Worlds End",
-                year: 2007,
-                ratings: {
-                  critics_score: 45,
-                  player_score: 0,
-                  actual_score: 45
-                }
-              },
-              {
-                id: 15,
-                title: "Indiana Jones and the Kingdom of the Crystal Skull",
-                year: 2008,
-                ratings: {
-                  critics_score: 78,
-                  player_score: 0,
-                  actual_score: 78
-                }
-              },
-              {
-                id: 16,
-                title: "The Dark Knight Rises",
-                year: 2012,
-                ratings: {
-                  critics_score: 88,
-                  player_score: 0,
-                  actual_score: 88
-                }
-              },
-              {
-                id: 17,
-                title: "The Amazing Spider-Man",
-                year: 2012,
-                ratings: {
-                  critics_score: 73,
-                  player_score: 0,
-                  actual_score: 73
-                }
-              },
-              {
-                id: 18,
-                year: 2009,
-                title: "The Ugly Truth",
-                ratings: {
-                  critics_score: 13,
-                  player_score: 0,
-                  actual_score: 13
-                }
-              },
-              {
-                id: 19,
-                title: "The Hunger Games",
-                year: 2012,
-                ratings: {
-                  critics_score: 84,
-                  player_score: 0,
-                  actual_score: 84
+                  player_score: 0
                 }
               }
+              // {
+                // id: 3,
+                // title: "The Incredibles",
+                // year: 2004,
+                // ratings: {
+                  // critics_score: 97,
+                  // player_score: 0,
+                  // actual_score: 97
+                  // }
+                // },
+              // {
+                // id: 4,
+                // title: "The Godfather",
+                // year: 1972,
+                // ratings: {
+                  // critics_score: 100,
+                  // player_score: 0,
+                  // actual_score: 100
+                // }
+              // },
+              // {
+                // id: 5,
+                // title: "Spaceballs",
+                // year: 1987,
+                // ratings: {
+                  // critics_score: 50,
+                  // player_score: 0,
+                  // actual_score: 50
+                // }
+              // },
+              // {
+                // id: 6,
+                // title: "Star Wars: Episode III - Revenge of the Sith 3D",
+                // year: 2005,
+                // ratings: {
+                  // critics_score: 80,
+                  // player_score: 0,
+                  // actual_score: 80
+                // }
+              // },
+              // {
+                // id: 7,
+                // title: "The Lord of the Rings: The Return of the King",
+                // year: 2003,
+                // ratings: {
+                  // critics_score: 94,
+                  // player_score: 0,
+                  // actual_score: 94
+                // }
+              // },
+              // {
+                // id: 8,
+                // title: "The Matrix",
+                // year: 1999,
+                // ratings: {
+                  // critics_score: 87,
+                  // player_score: 0,
+                  // actual_score: 87
+                // }
+              // },
+              // {
+                // id: 9,
+                // title: "Transformers: Revenge of the Fallen",
+                // year: 2009,
+                // ratings: {
+                  // critics_score: 19,
+                  // player_score: 0,
+                  // actual_score: 19
+                // }
+              // },
+              // {
+                // id: 10,
+                // title: "The Bourne Ultimatum",
+                // year: 2007,
+                // ratings: {
+                  // critics_score: 94,
+                  // player_score: 0,
+                  // actual_score: 94
+                // }
+              // },
+              // {
+                // id: 11,
+                // title: "Harry Potter and the Order of the Phoenix",
+                // year: 2007,
+                // ratings: {
+                  // critics_score: 79,
+                  // player_score: 0,
+                  // actual_score: 79
+                // }
+              // },
+              // {
+                // id: 12,
+                // title: "Shrek the Third",
+                // year: 2007,
+                // ratings: {
+                  // critics_score: 40,
+                  // player_score: 0,
+                  // actual_score: 40
+                // }
+              // },
+              // {
+                // id: 13,
+                // title: "Sweeney Todd: The Demon Barber of Fleet Street",
+                // year: 2007,
+                // ratings: {
+                  // critics_score: 86,
+                  // player_score: 0,
+                  // actual_score: 86
+                // }
+              // },
+              // {
+                // id: 14,
+                // title: "Pirates of the Caribbean: At Worlds End",
+                // year: 2007,
+                // ratings: {
+                  // critics_score: 45,
+                  // player_score: 0,
+                  // actual_score: 45
+                // }
+              // },
+              // {
+                // id: 15,
+                // title: "Indiana Jones and the Kingdom of the Crystal Skull",
+                // year: 2008,
+                // ratings: {
+                  // critics_score: 78,
+                  // player_score: 0,
+                  // actual_score: 78
+                // }
+              // },
+              // {
+                // id: 16,
+                // title: "The Dark Knight Rises",
+                // year: 2012,
+                // ratings: {
+                  // critics_score: 88,
+                  // player_score: 0,
+                  // actual_score: 88
+                // }
+              // },
+              // {
+                // id: 17,
+                // title: "The Amazing Spider-Man",
+                // year: 2012,
+                // ratings: {
+                  // critics_score: 73,
+                  // player_score: 0,
+                  // actual_score: 73
+                // }
+              // },
+              // {
+                // id: 18,
+                // year: 2009,
+                // title: "The Ugly Truth",
+                // ratings: {
+                  // critics_score: 13,
+                  // player_score: 0,
+                  // actual_score: 13
+                // }
+              // },
+              // {
+                // id: 19,
+                // title: "The Hunger Games",
+                // year: 2012,
+                // ratings: {
+                  // critics_score: 84,
+                  // player_score: 0,
+                  // actual_score: 84
+                // }
+              // }
             ]
