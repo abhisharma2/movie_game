ecb1fc979f3cfe1999d5cc6a981401558a5e6ab4
Abhi Sharma <abhisharma2@gmail.com>
2014-09-13 07:40:09 -0400

    bind port to 9000

--- web.js
+++ web.js
@@ -1,9 +1,7 @@
-var express = require('express');
 var gzippo = require('gzippo');
+var express = require('express');
 var app = express();
 
 app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(process.env.PORT || 9000);
